511629375ba7c8aa94fd95dc3e5c0ecdad69bc94
daichi-iwamoto <tay1fai0y@gmail.com>
2020-07-14 21:36:57 +0900

    vuefire導入 リアルタイム更新対応

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,6 +28,7 @@ export default {
   ** Plugins to load before mounting the App
   */
   plugins: [
+    '~/plugins/vuefire'
   ],
   /*
   ** Nuxt.js dev-modules
--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
     <div class="logo-box">
       <logo />
     </div>
-    <button @click="submit()" :class="{ stay: flag }" class="submit-btn">
+    <button @click="submit()" :class="{ stay: flag.state }" class="submit-btn">
       {{ btnTxt }}
     </button>
   </div>
@@ -23,55 +23,42 @@ export default {
   data () {
     return {
       btnTxt: 'いなくなった！',
-      flag: false
+      flag: {}
+    }
+  },
+  firestore () {
+    return {
+      flag: firebase.firestore().collection('leave-time').doc('IZkrWRg1loXHz6gKHX8E')
     }
   },
   mounted () {
-    const db = firebase.firestore()
-    const status = db.collection('leave-time').doc('IZkrWRg1loXHz6gKHX8E')
-
-    status.get().then((doc) => {
-      if (doc.exists) {
-        if (doc.data().state === true) {
-          this.btnTxt = 'せきにいるよ'
-          this.flag = true
-        } else {
-          this.btnTxt = 'いなくなった！'
-          this.flag = false
-        }
-      } else {
-        console.log('error')
-      }
-    }).catch((error) => {
-      console.log('api error :' + error)
-    })
+    if (this.flag.state === true) {
+      this.btnTxt = 'せきにいるよ'
+    } else {
+      this.btnTxt = 'いなくなった！'
+    }
+  },
+  beforeUpdate () {
+    if (this.flag.state === true) {
+      this.btnTxt = 'せきにいるよ'
+    } else {
+      this.btnTxt = 'いなくなった！'
+    }
   },
   methods: {
     submit () {
       const db = firebase.firestore()
       const status = db.collection('leave-time').doc('IZkrWRg1loXHz6gKHX8E')
 
-      status.get().then((doc) => {
-        if (doc.exists) {
-          if (doc.data().state === true) {
-            status.set({
-              state: false
-            })
-            this.btnTxt = 'いなくなった！'
-            this.flag = false
-          } else {
-            status.set({
-              state: true
-            })
-            this.btnTxt = 'せきにいるよ'
-            this.flag = true
-          }
-        } else {
-          console.log('error')
-        }
-      }).catch((error) => {
-        console.log('api error :' + error)
-      })
+      if (this.flag.state === true) {
+        status.set({
+          state: false
+        })
+      } else {
+        status.set({
+          state: true
+        })
+      }
     }
   }
 }
